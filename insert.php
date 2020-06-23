86b5e54c5cc7a8aaa0270c2c2a0d3aaafecaf68f
Meghana <lathamithu2000@gmail.com>
2020-06-24 03:22:55 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -14,11 +14,18 @@ $Date_ = $_POST["date_"];
 $Gender = $_POST["gender"];
 $Education = $_POST["education"];
 $Address = $_POST["address"];
+$file = $_FILES["file"];
+$file_name = $file['name'];
+$file_type = $file ['type'];
+$file_size = $file ['size'];
+$file_path = $file ['tmp_name'];
 
-$sql = "INSERT INTO `form`(`First name`, `Last name`, `DOB`, `Gender`, `Qualification`, `Address`) VALUES (:fname,:lname,:date_,:gender,:education,:address)";
+if(move_uploaded_file ($file_path,$file_name))
+{
+	$sql = "INSERT INTO `form`(`First name`, `Last name`, `DOB`, `Gender`, `Qualification`, `Address`, `Profile`) VALUES (:fname,:lname,:date_,:gender,:education,:address,:file)";
 $stmt = $pdo->prepare($sql);
 
-$pdoExec = $stmt->execute(array(":fname"=>$Firstname,":lname"=>$Lastname,":date_"=>$Date_,":gender"=>$Gender,":education"=>$Education,":address"=>$Address));
+$pdoExec = $stmt->execute(array(":fname"=>$Firstname,":lname"=>$Lastname,":date_"=>$Date_,":gender"=>$Gender,":education"=>$Education,":address"=>$Address,":file"=>$file_name));
 
 if($pdoExec)
 {
@@ -28,5 +35,8 @@ else
 {
 	echo "Not inserted";
 }
+}
+
+
 
 ?>
