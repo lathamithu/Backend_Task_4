16f24276c4f40da660d91e9f21f360a924690cb2
Meghana <lathamithu2000@gmail.com>
2020-06-24 21:58:21 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -20,12 +20,13 @@ $file_type = $file ['type'];
 $file_size = $file ['size'];
 $file_path = $file ['tmp_name'];
 
+
 if(move_uploaded_file ($file_path,$file_name))
 {
-	$sql = "INSERT INTO `form`(`First name`, `Last name`, `DOB`, `Gender`, `Qualification`, `Address`, `Profile`) VALUES (:fname,:lname,:date_,:gender,:education,:address,:file)";
+	$sql = "INSERT INTO `form`(`First name`, `Last name`, `DOB`, `Gender`, `Qualification`, `Address`, `Filename`, `Filetype`, `Filesize`,`Filepath`) VALUES (:fname,:lname,:date_,:gender,:education,:address,:filename,:filetype,:filesize,:filepath)";
 $stmt = $pdo->prepare($sql);
 
-$pdoExec = $stmt->execute(array(":fname"=>$Firstname,":lname"=>$Lastname,":date_"=>$Date_,":gender"=>$Gender,":education"=>$Education,":address"=>$Address,":file"=>$file_name));
+$pdoExec = $stmt->execute(array(":fname"=>$Firstname,":lname"=>$Lastname,":date_"=>$Date_,":gender"=>$Gender,":education"=>$Education,":address"=>$Address,":filename"=>$file_name,":filetype"=>$file_type,":filesize"=>$file_size,":filepath"=>$file_path));
 
 if($pdoExec)
 {
